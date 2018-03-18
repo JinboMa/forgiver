2ad78042ed851bb1c7b50c91c505b028eee96b55
jinboMa <596756194@qq.com>
2018-03-18 17:30:10 +0800

    change app

---
+++ src/app/App.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+#app
+  #nav
+    router-link(to="/") Home
+  router-view
+</template>
+
+<script lang="ts">
+import './app.styl'
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class App extends Vue {
+}
+</script>
